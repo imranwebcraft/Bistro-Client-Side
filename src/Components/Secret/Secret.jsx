7d1d1c28bb4502f3e82466c7b15db3f4fc39eb97
imranwebcraft <imranhossainsakil22@gmail.com>
2023-11-15 12:08:13 -0800

    Setup new windows, test command

--- src/Components/Secret/Secret.jsx
+++ src/Components/Secret/Secret.jsx
@@ -1,7 +1,7 @@
 const Secret = () => {
 	return (
 		<div className=" h-screen flex justify-center items-center text-5xl font-medium">
-			<h3>Secret</h3>
+			<h3>Secret Things</h3>
 		</div>
 	);
 };
